b3c4f9078e85525ab7b36a11dedf25dabd8649ef
Tashdik <s2110477153@ru.ac.bd>
2024-06-06 02:02:50 +0600

    pagination complete

--- src/pages/Dashboard/admin/AllUsers.jsx
+++ src/pages/Dashboard/admin/AllUsers.jsx
@@ -1,24 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import AdminAllUsersTable from "../../../components/tables/AdminAllUsersTable";
+import { useEffect, useState } from "react";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 const AllUsers = () => {
 
+    const [currentPage, setCurrentPage] = useState(0);
+    const [count, setCount] = useState(0);
+
+    const axiosPublic = useAxiosPublic();
+
+    useEffect(() => {
+        axiosPublic.get('/usersCount')
+            .then(res => {
+                console.log('Count = ', res.data.count);
+                setCount(res.data.count)
+            })
+
+    }, [])
+
+    const numberOfPages = Math.ceil(count / 5);
+    const pages = [...Array(numberOfPages).keys()];
+
     const axiosSecure = useAxiosSecure();
 
-    const {data: users=[], refetch} = useQuery({
-        queryKey: ['users'],
-        queryFn: async () =>{
-            const res = await axiosSecure.get('/users');
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users', currentPage],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users?page=${currentPage}`);
 
             return res.data;
         }
     })
 
     return (
-        <div>
+        <div className="relative h-screen ">
             Total users = {users.length}
             <AdminAllUsersTable users={users} refetch={refetch}></AdminAllUsersTable>
+            <div className="space-x-2 absolute bottom-5 border w-full flex justify-center">
+                {
+                    pages.map(i => <button className={currentPage === i ? 'bg-blue-500 px-3 py-2 rounded-md  ' : 'px-3 py-2 rounded-md border border-gray-400'} onClick={() => setCurrentPage(i)} key={i}>{i}</button>)
+                }
+            </div>
         </div>
     );
 };
