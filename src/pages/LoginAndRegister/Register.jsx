500be4b2ace940005ae3dadf532990c020d523bc
Tashdik <s2110477153@ru.ac.bd>
2024-06-09 14:53:13 +0600

    reagister page now fully completed

--- src/pages/LoginAndRegister/Register.jsx
+++ src/pages/LoginAndRegister/Register.jsx
@@ -12,6 +12,8 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { imageUpload } from "../../utils/imageURL";
 import { useState } from "react";
 
+import image from '../../assets/reg.jpg'
+
 const Register = () => {
 
     const [imagePreview, setImagePreview] = useState();
@@ -98,22 +100,24 @@ const Register = () => {
 
 
     return (
-        <section className="bg-white">
+        <section className="bg-white lg:flex">
             <Helmet>
                 <title>Sign Up || Ship Ninja</title>
             </Helmet>
-            <div className="container flex flex-col items-center justify-center min-h-screen px-6 mx-auto">
-                <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
-                    <div className="flex justify-center mx-auto">
+            <div className="lg:w-1/3 flex flex-col items-center justify-center min-h-screen px-6 py-4 mx-auto">
+                <div className="flex justify-center my-2">
+                    <SocialLogin></SocialLogin>
+                </div>
+                <div className="divider divider-neutral">Or</div>
+                <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md border-2 border-black p-4">
+                    {/* <div className="flex justify-center mx-auto">
                         <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="Logo" />
-                    </div>
+                    </div> */}
 
-                    <div className="flex items-center justify-center mt-6">
-
-
-                        <a href="#" className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400">
+                    <div className="flex items-center justify-center">
+                        <h1 className="w-1/3 pb-4 text-center text-blue-400 font-semibold capitalize border-b-2 border-orange-500 dark:border-orange-400">
                             sign up
-                        </a>
+                        </h1>
                     </div>
 
                     <div className="relative flex items-center mt-8">
@@ -128,10 +132,7 @@ const Register = () => {
                     {errors.name && <span className="text-red-600 font-bold">Name is required</span>}
 
                     <div className="relative flex items-center mt-8">
-                        {/* <span className="absolute">
-                            <MdOutlineAddPhotoAlternate className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
-                        </span>
-                        <input type="file" {...register("photo", { required: true })} className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11  dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 " placeholder="Upload Photo" /> */}
+                        
 
 
                         {/* ******** */}
@@ -223,17 +224,10 @@ const Register = () => {
                     {errors.password?.type === 'pattern' && <span className="text-red-600 font-bold">Password must contain at least one Uppercase and Special character</span>}
 
 
-                    {/* <div className="relative flex items-center mt-4">
-                        <span className="absolute">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                            </svg>
-                        </span>
-                        <input type="password" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Confirm Password" />
-                    </div> */}
+                    
 
                     <div className="mt-6">
-                        <input className="w-full px-6 py-3 text-sm font-medium bg-blue-500 rounded-lg text-white cursor-pointer" type="submit" value="Sign Up" />
+                        <input className="w-full px-6 py-3 text-sm font-medium bg-blue-500 hover:bg-blue-400 rounded-lg text-white cursor-pointer" type="submit" value="Sign Up" />
 
                         <div className="mt-6 text-center">
                             <Link to='/login' className="text-sm text-blue-500 hover:underline dark:text-blue-400">
@@ -243,9 +237,10 @@ const Register = () => {
 
                     </div>
                 </form>
-                <div className="flex justify-center mt-2">
-                    <SocialLogin></SocialLogin>
-                </div>
+
+            </div>
+            <div className="lg:w-1/2 flex items-center">
+                <img className="w-3/4" src={image} alt="" />
             </div>
         </section>
     );
