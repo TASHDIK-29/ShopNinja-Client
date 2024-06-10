95333c5d8ff0df43352d3f9d8011b728e60888b2
Tashdik <s2110477153@ru.ac.bd>
2024-06-10 21:08:03 +0600

    parallax added to banner

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -1,17 +1,25 @@
+import { Parallax } from 'react-parallax';
 import './banner.css'
-
+import img from '../../../assets/banner.jpg'
 
 const Banner = () => {
     return (
-        <div className='banner-container'>
-            <div className='md:w-3/5 h-full flex flex-col justify-center space-y-6 p-8'>
-                <h1 className='text-white text-2xl md:text-5xl font-semibold  text-gradient'>We Deliver Your Goods With <br /> Passion & Love</h1>
-                <h3 className='text-white text-lg'>Over 100++ Acer Interior Warehouse Space</h3>
-                <div className='border h-8 w-2/3 relative '>
-                    <button className='text-white text-lg font-semibold absolute right-2  text-gradient'>Search</button>
+        <Parallax
+            blur={{ min: -10, max: 10 }}
+            bgImage={img}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <div className='banner-container'>
+                <div className='md:w-3/5 h-full flex flex-col justify-center space-y-6 p-8'>
+                    <h1 className='text-white text-2xl md:text-5xl font-semibold  text-gradient'>We Deliver Your Goods With <br /> Passion & Love</h1>
+                    <h3 className='text-white text-lg'>Over 100++ Acer Interior Warehouse Space</h3>
+                    <div className='border h-8 w-2/3 relative '>
+                        <button className='text-white text-lg font-semibold absolute right-2  text-gradient'>Search</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
     );
 };
 
